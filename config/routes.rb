4e1e73fff81f504ba16fde0a9697144950f9a138
keiichiro <keiichiro.material@gmail.com>
2021-01-24 21:32:19 +0100

    tentative page for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'staticaa_pages/home'
   get 'staticaa_pages/about'
   get 'staticaa_pages/help'
+  root 'application#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
